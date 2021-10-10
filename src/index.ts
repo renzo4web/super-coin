1463b9bf6b9fd1124b587800e7e44e2ae71e70c8
Renzo Barrios <renzobarrios@aol.com>
2021-10-10 01:00:47 -0300

    initial commit :tada:

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-export * from './lib/async';
-export * from './lib/number';
+import { BlockChain } from './lib/BlockChain';
+import hash from 'object-hash';
+
+let blockChain = new BlockChain();
+
+// This should be a complicated algo to check
+const PROOF = 125;
+
+const validProof = (proof) => {
+  let guessHas = hash(proof);
+};
---
+++ src/lib/BlockChain.ts
@@ -0,0 +1,101 @@
+/**
+ * A sample async function (to demo Typescript's es7 async/await down-leveling).
+ *
+ * ### Chain (es imports)
+ * ```
+ * const chain = block[]
+ * ```
+ *
+ * ### Example (commonjs)
+ * ```js
+ * var double = require('typescript-starter').asyncABC;
+ * asyncABC().then(console.log);
+ * // => ['a','b','c']
+ * ```
+ *
+ * @returns a Promise which should contain `['a','b','c']`
+ */
+
+import hash from 'object-hash';
+
+interface IBlock {
+  index: number;
+  transactions: any[];
+  prevHash: string;
+}
+
+class Block {
+  private _index: number;
+  private _blockstamp: number;
+  private _transactions: any[];
+  private _hash: string;
+  private _prevHash: string;
+
+  constructor(readonly blockProps: IBlock) {
+    this._index = blockProps.index;
+    this._blockstamp = Date.now();
+    this._transactions = blockProps.transactions;
+    this._hash = hash(blockProps);
+    this._prevHash = blockProps.prevHash;
+  }
+
+  get index() {
+    return this._index;
+  }
+
+  get blockstamp() {
+    return this._blockstamp;
+  }
+  get transactions() {
+    return this._transactions;
+  }
+  get hash() {
+    return this._hash;
+  }
+  get prevHash() {
+    return this._prevHash;
+  }
+}
+
+export class BlockChain {
+  private chain: any[];
+  private currentTransactions: any[];
+
+  constructor() {
+    this.chain = [];
+    // Transactions
+    this.currentTransactions = [];
+  }
+
+  addNewBlock(prevHash: string) {
+    let block = new Block({
+      index: this.chain.length + 1,
+      prevHash,
+      transactions: this.currentTransactions,
+    });
+
+    // Add block to the chain
+    this.chain.push(block);
+    // Reset currentTransactions
+    this.currentTransactions = [];
+    return block;
+  }
+
+  addNewTransaction(sender: string, recipient: string, amount: number) {
+    let transaction = {
+      sender,
+      recipient,
+      amount,
+    };
+
+    this.currentTransactions.push(transaction);
+  }
+
+  get lastBlock() {
+    return this.chain.slice(-1)[0];
+  }
+
+  get isChainEmpty() {
+    return this.chain.length === 0;
+  }
+}
